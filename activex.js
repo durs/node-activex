b1f6eaf77c8a8d943a1b0a8909718fe5af067461
durs <yuri.dursin@gmail.com>
2021-08-10 09:19:05 +0300

    fix require node

--- activex.js
+++ activex.js
@@ -1,4 +1,4 @@
-var ActiveX = module.exports = require('./build/Release/node_activex');
+var ActiveX = module.exports = require('./build/Release/node_activex.node');
 
 global.ActiveXObject = function(id, opt) {
     return new ActiveX.Object(id, opt);
